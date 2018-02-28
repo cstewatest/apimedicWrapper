1cc37b5b66e91c95fe31000acc3c89f3ec7401bc
Christina Stewart <christina.v.stewart@gmail.com>
2018-02-28 16:03:00 +0900

    promisify authenticator and throw compatible error to controller

--- api/services/Authenticator.js
+++ api/services/Authenticator.js
@@ -4,25 +4,35 @@ let redisClient = require('../models/redisClient');
 
 const BASE_URL = "https://sandbox-authservice.priaid.ch/login"
 const API_KEY = "christina.v.stewart@gmail.com"
-const SECRET_KEY = redisClient.getAsync("secretKey").then((k) => {return k})
+
+class AuthError extends Error {
+  constructor(message) {
+    super(message);
+    this.status = 500;
+    this.responseText = message;
+  }
+}
 
 module.exports = function Authenticator() {  
-  let that = this;
-  this.computedHash = CryptoJS.HmacMD5(BASE_URL, SECRET_KEY);
-  this.computedHashString = this.computedHash.toString(CryptoJS.enc.Base64);
-
   this.call = () => {
-    let bearer = API_KEY + ":" + that.computedHashString
-    
     return (
-      new Requester().call('POST', BASE_URL, {bearer: bearer}).then(function(apiMedicResponse) {
+      redisClient.getAsync("secretKey").then((secretKey) => {
+        let computedHash = CryptoJS.HmacMD5(BASE_URL, secretKey);
+        let computedHashString = computedHash.toString(CryptoJS.enc.Base64);
+        return (API_KEY + ":" + computedHashString)
+      }).then(function(bearer) {
+        return (new Requester().call('POST', BASE_URL, {bearer: bearer}))
+      }).then(function(apiMedicResponse) {
         let result = apiMedicResponse.responseText;
         let parsedResult = JSON.parse(result)
         let token = parsedResult["Token"]
         redisClient.set('authToken', token)
         redisClient.expire('authToken', 7200)
         return token
-      }).catch(err => {({status: err.status, response: err.responseText})})
+      }).catch((err) => {
+        console.log(err);
+        throw new AuthError("Error authenticating. Please try again later")
+      })
     )
   }
 }
